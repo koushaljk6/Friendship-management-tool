b499088543823a0f70d50d8c83ab9a34f3aab2ad
Koushal kumar <koushalk675@gmail.com>
2025-03-22 23:29:20 +0530

    2-ui design completed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+    
+  </React.StrictMode>,
+)
