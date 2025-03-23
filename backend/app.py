69f6be6259ba9e88cf5fe645b00916cc2b611143
Koushal kumar <koushalk675@gmail.com>
2025-03-23 16:53:44 +0530

    bug fixing of Post mthod

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,7 @@ def index(filename):
 import routes
 
 with app.app_context():
-    db.create_all()
+      db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+      app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -18,7 +18,7 @@ def create_friend():
         #validations
         required_fields = ["name", "role", "description", "gender"]
         for field in required_fields:
-            if field not in data or data.get(field):
+            if field not in data or not data.get(field):
                 return jsonify({"error":f"Missing required field: {field}"}), 400
 
         name = data.get("name")
